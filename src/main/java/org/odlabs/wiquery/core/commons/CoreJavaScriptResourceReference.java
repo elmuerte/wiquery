458693344a7bb4adade7b576b64c327b3df20157
Michiel Hendriks <michiel.hendriks@mp-objects.com>
2015-03-06 11:41:56 +0100

    upgraded to jquery 1.7.2

--- src/main/java/org/odlabs/wiquery/core/commons/CoreJavaScriptResourceReference.java
+++ src/main/java/org/odlabs/wiquery/core/commons/CoreJavaScriptResourceReference.java
@@ -1,16 +1,16 @@
 /*
  * Copyright (c) 2009 WiQuery team
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -22,37 +22,35 @@
 package org.odlabs.wiquery.core.commons;
 
 /**
- * $Id: CoreJavaScriptResourceReference.java 81 2009-05-28 20:05:12Z
- * lionel.armanet $
+ * $Id: CoreJavaScriptResourceReference.java 81 2009-05-28 20:05:12Z lionel.armanet $
  * <p>
  * References the core jQuery library.
  * </p>
- * 
+ *
  * @author Lionel Armanet
  * @since 0.5
  */
-public class CoreJavaScriptResourceReference extends
-		WiQueryJavaScriptResourceReference {
-
-	private static final long serialVersionUID = -2918665261694523156L;
+public class CoreJavaScriptResourceReference extends WiQueryJavaScriptResourceReference {
 
 	/**
 	 * Singleton instance.
 	 */
-	private static CoreJavaScriptResourceReference instance = new CoreJavaScriptResourceReference();;
+	private static CoreJavaScriptResourceReference instance = new CoreJavaScriptResourceReference();
+
+	private static final long serialVersionUID = -2918665261694523156L;;
+
+	/**
+	 * Builds a new instance of {@link CoreJavaScriptResourceReference}.
+	 */
+	private CoreJavaScriptResourceReference() {
+		super(CoreJavaScriptResourceReference.class, "jquery/jquery-1.7.2.js");
+	}
 
 	/**
 	 * Returns the {@link CoreJavaScriptResourceReference} instance.
 	 */
 	public static CoreJavaScriptResourceReference get() {
 		return instance;
-	}
-
-	/**
-	 * Builds a new instance of {@link CoreJavaScriptResourceReference}.
-	 */
-	private CoreJavaScriptResourceReference() {
-		super(CoreJavaScriptResourceReference.class, "jquery/jquery-1.5.1.js");
 	}
 
 }
